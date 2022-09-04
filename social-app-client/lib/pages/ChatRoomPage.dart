de9a2936e48f8fde0d9a9076d6fa099d95a50d5a
Muhammad Aneek Rahman <reflection.active@gmail.com>
2022-09-05 05:03:56 +0600

    - Fix ChatRoomPage popup block/unblock button

--- social-app-client/lib/pages/ChatRoomPage.dart
+++ social-app-client/lib/pages/ChatRoomPage.dart
@@ -171,6 +171,7 @@ class ChatTopBar extends StatelessWidget {
             ),
             Text(
               "@" + otherUser.userName!,
+              // "Last message " + convertToTimeAgo(new DateTime.fromMillisecondsSinceEpoch(int.parse(chatRow!.lastMsgSentTime!))) + " ago",
               overflow: TextOverflow.ellipsis,
               style: TextStyle(fontFamily: HelveticaFont.Roman, fontSize: 12, color: Colors.black38),
             ),
@@ -271,7 +272,7 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
                           ? _buildMessagesStreamBuilder(context)
                           : Center(child: Text("You haven't messaged yet!"))
                       : _buildLoadingAnim(),
-                  widget.chatRow != null && (widget.chatRow!.requestedByOtherUser! || widget.chatRow!.blockedByThisUser!)
+                  widget.chatRow != null
                       ? ChatRequestActions(
                           key: _chatRequestActionsGlobalKey,
                           chatRow: widget.chatRow!,
@@ -420,74 +421,78 @@ class _ChatRequestActionsState extends State<ChatRequestActions> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          margin: EdgeInsets.only(top: 10),
-          height: 1,
-          width: MediaQuery.of(context).size.width,
-          color: Color(0xFFF1F1F1F1),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
-          child: Text(
-            widget.chatRow.blockedByThisUser!
-                ? "You have blocked this user and they will not be able to message you."
-                : "This message will be moved to your chat list when you accept it or reply here.",
-            style: TextStyle(fontFamily: HelveticaFont.Light),
+    if (widget.chatRow.requestedByOtherUser! || widget.chatRow.blockedByThisUser!) {
+      return Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            height: 1,
+            width: MediaQuery.of(context).size.width,
+            color: Color(0xFFF1F1F1F1),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
-          child: !_loading
-              ? Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        _acceptRequest();
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF3EC2F9),
-                          border: Border.all(color: Colors.black12, width: 1),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-                        child: Text(
-                          "Accept Request",
-                          style: TextStyle(
-                            fontFamily: HelveticaFont.Bold,
-                            color: Colors.white,
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: Text(
+              widget.chatRow.blockedByThisUser!
+                  ? "You have blocked this user and they will not be able to message you."
+                  : "This message will be moved to your chat list when you accept it or reply here.",
+              style: TextStyle(fontFamily: HelveticaFont.Light),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: !_loading
+                ? Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          _acceptRequest();
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF3EC2F9),
+                            border: Border.all(color: Colors.black12, width: 1),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                          child: Text(
+                            "Accept Request",
+                            style: TextStyle(
+                              fontFamily: HelveticaFont.Bold,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        blockUnblockUser();
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          border: Border.all(color: Colors.black12, width: 1),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-                        child: Text(
-                          widget.chatRow.blockedByThisUser! ? "Unblock User" : "Block User",
-                          style: TextStyle(
-                            fontFamily: HelveticaFont.Bold,
-                            color: Colors.white,
+                      GestureDetector(
+                        onTap: () {
+                          blockUnblockUser();
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            border: Border.all(color: Colors.black12, width: 1),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                          child: Text(
+                            widget.chatRow.blockedByThisUser! ? "Unblock User" : "Block User",
+                            style: TextStyle(
+                              fontFamily: HelveticaFont.Bold,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                )
-              : _buildLoadingAnim(),
-        ),
-      ],
-    );
+                    ],
+                  )
+                : _buildLoadingAnim(),
+          ),
+        ],
+      );
+    } else {
+      return SizedBox();
+    }
   }
 }
